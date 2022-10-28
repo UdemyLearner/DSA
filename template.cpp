105e1b5579ed35641cca935e89c81f0532ea7d64
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-10-29 00:40:15 +0530

    Changes to be committed: 	new file:   .fleet/run.json 	modified:   .gitignore 	new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	modified:   template.cpp

--- template.cpp
+++ template.cpp
@@ -1,32 +1,54 @@
-vector<int> bs(vector<int> &nums, int target)
+#include <iostream>
+#include <vector>
+using namespace std;
+void rotate(vector<vector<int>> &matrix)
 {
-    int s = 0, e = nums.size() - 1;
-    int m = s + (e - s) / 2;
-    int f = -1, l = -1;
-    while (s < e)
+    vectors<int> ans;
+    int Row = matrix.size();
+    int Col = matrix[0].size();
+    for (int col = 0; col < Col; col++)
     {
-        if (target == nums[m])
+        if (col & 1)
         {
-            f = m;
-            if (nums[m - 1] == target)
+            // odd Index -> Bottom to top
+            for (int row = 0; row < Row; row++)
             {
-                f = m - 1;
-                l = m;
+                ans.push_back(matrix[row][col]);
             }
-            if (nums[m + 1] == target)
-            {
-                l = m + 1;
-            }
-        }
-        if (nums[m] < target)
-        {
-            s = m + 1;
         }
         else
         {
-            e = m - 1;
+            for (int row = Row - 1; row >= 0; row--)
+            {
+                ans.push_back(matrix[row][col]);
+            }
         }
-        m = s + (e - s) / 2;
     }
-    return {f, l};
+    for (int row = 0; row < 9; row++)
+    {
+        cout << ans[row];
+        cout << endl;
+    }
+
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            cout << matrix[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl
+         << "Printing the array after rotation" << endl;
+
+    rotate(matrix);
+    // print
+
+    return 0;
 }
