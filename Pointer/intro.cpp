06a8f6099d666fab572717e01e7c7682549021be
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-10-30 05:11:14 +0530

    Changes to be committed: 	new file:   Pointer/intro.cpp 	new file:   a.cpp

---
+++ Pointer/intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num = 5;
+    cout<<"Value of num variable is "<<num<<endl;
+    cout<<"Address of num Variable "<<&num;
+    return 0;
+}
