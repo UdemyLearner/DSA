cdf489417ce99ec1d16e6685cb04d1b96ca829aa
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-10-30 12:07:56 +0530

    Changes to be committed: 	modified:   Pointer/intro.cpp 	new file:   Pointer/playing_with_pointers.cpp

--- Pointer/intro.cpp
+++ Pointer/intro.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int num = 5;
-    cout<<"Value of num variable is "<<num<<endl;
-    cout<<"Address of num Variable "<<&num;
+    cout << "Value of num variable is " << num << endl;
+    cout << "Address of num Variable " << &num << endl;
+
+    int *p = &num;
+    cout << "Address to which pointer p is pointing to is " << p << endl
+         << " value of pointer p which variable it is pointing to " << *p;
+
     return 0;
 }
