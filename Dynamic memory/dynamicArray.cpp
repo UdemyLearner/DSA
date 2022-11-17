fab445463c18ac93aac6fbdb5f5307fd2a18b268
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2022-11-17 13:51:59 +0000

    Changes to be committed: 	modified:   Dynamic memory/dynamicArray.cpp

--- Dynamic memory/dynamicArray.cpp
+++ Dynamic memory/dynamicArray.cpp
@@ -6,31 +6,56 @@ memory -> Stack = small size    => Static  memory Allocation  >> int a;
        -> heap  = larger size   => Dynamic memroy Allocation  >> char *ch =  new char;    << "new char" return address of char var crated in heap memory
 */
 
-int getSum(int *arr, int n) {
+int getSum(int *arr, int n)
+{
 
     int sum = 0;
-    for(int i=0; i<n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         sum += arr[i];
-    } 
+    }
     return sum;
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
     // char ch = 'g';
     // cout << sizeof(ch) << endl;
     // char *c = &ch;
     // cout << sizeof(c) << endl;
-    int n ;
-    cin>>n;
+    int n;
+    cin >> n;
     int *arr = new int[n];
 
     for (size_t i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    
-    int ans = getSum(arr,n);
-    cout<<ans<<endl;
+
+    int ans = getSum(arr, n);
+    cout << ans << endl;
+
+    while (true)
+    {
+        int i = 5;
+    }
+
+    while (true)
+    {
+        // this is dangerous reasone stated below
+        int *ptr = new int;
+        int *ptr1 = new int;
+        int *ptr11 = new int;
+        int *ptr111 = new int;
+        int *ptr1111 = new int;
+ 
+    }
+
+/*    
+    delete i;
+    delete []arr; 
+
+    used to free heap memory , as heap memory is not free automatically
+*/            
     return 0;
 }
