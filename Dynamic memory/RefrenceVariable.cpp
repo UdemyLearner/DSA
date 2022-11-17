0807bcffca3857147fc709e3b54e5e2cc7f6f8ec
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2022-11-17 13:00:46 +0000

    Changes to be committed: 	modified:   Dynamic memory/RefrenceVariable.cpp

--- Dynamic memory/RefrenceVariable.cpp
+++ Dynamic memory/RefrenceVariable.cpp
@@ -1,14 +1,14 @@
 #include "iostream"
 using namespace std;
 
-void update(int n)
-{
-    n++;
+void update(int n)      //Pass by Value
+{                       // Copy of i is created and stored in n varable 
+    n++;                // alag sa meory create hogi hor n variable
 }
 
-void updateRef(int &n)
-{
-    n++;
+void updateRef(int &n)  //Pass by Refrence
+{                       // &n is same memory block as i variable
+    n++;                // alag sa koi memory block nahi use hoga
 }
 
 int main(int argc, char const *argv[])
