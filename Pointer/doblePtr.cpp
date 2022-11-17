88d467dace8ca4c678e4e6e44c9793fb1692a15d
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2022-11-17 04:41:32 +0000

    Changes to be committed: 	modified:   Pointer/doblePtr.cpp

--- Pointer/doblePtr.cpp
+++ Pointer/doblePtr.cpp
@@ -6,15 +6,22 @@ int main()
     int *p1 = &i;
     int **p2 = &p1;
 
-    cout << "Address of p1 ->" << p1 << endl;
-    cout << "Address of p2 ->" << p2 << endl;
+    cout<< "Memory Location of i "<<&i<<endl;
+    cout << "Pointing of Pointer p1 ->" << p1 << endl;
+    cout << "Pointing of Pointer p2 ->" << p2 << endl;
 
-    cout << "value at p1 ->" << *p1 << endl;
+    //cout << "value at p1 ->" << *p1 << endl;
     cout << "value at p2 ->" << *p2 << endl;
 
-    cout << i << endl
+    
+    cout << "Address of p1 ->" << &p1 << endl;
+    cout << "Address of p2 ->" << &p2 << endl;
+
+    cout <<"Values at "<< i << endl
          << *p1 << endl
          << **p2 << endl;
+
+    
 
     return 0;
 }
