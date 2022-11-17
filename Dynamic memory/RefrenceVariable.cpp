7b934f981cdb307dba3683c0d2517c76fbb0658c
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2022-11-17 12:56:39 +0000

    modified:   .gitignore 	modified:   Dynamic memory/RefrenceVariable.cpp

--- Dynamic memory/RefrenceVariable.cpp
+++ Dynamic memory/RefrenceVariable.cpp
@@ -1,9 +1,36 @@
 #include "iostream"
 using namespace std;
 
-int main(int argc, char const *argv[])
+void update(int n)
 {
-    /* code */
-    return 0;
+    n++;
 }
 
+void updateRef(int &n)
+{
+    n++;
+}
+
+int main(int argc, char const *argv[])
+{
+    int i = 5;
+    int &j = i; // J is refrence varibale of I
+    cout << i << endl;
+    i++;
+    cout << i << endl;
+    j++;
+    cout << i << endl;
+
+    cout << "Memory of i " << &i << endl
+         << "Memory of i " << &j << endl;
+
+    cout << "before update() -> " << i << endl;
+    update(i);
+    cout << "after  update() -> " << i << endl;
+    cout << "-------------------------------" << endl;
+    cout << "before updateRef() -> " << i << endl;
+    updateRef(i);
+    cout << " after updateRef() -> " << i << endl;
+
+    return 0;
+}
