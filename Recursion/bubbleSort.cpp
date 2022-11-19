753a1bbbb6ea88aa0fb8ffa99eb83bcbd2e8d585
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-19 18:26:42 +0530

    Changes to be committed: 	new file:   Recursion/bubbleSort.cpp

---
+++ Recursion/bubbleSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void sortArray(int *arr, int n)
+{
+    // base case
+    if (n == 0 || n == 1)
+        return ;
+    // ek case solve karliya   => laregest element ko last m pahucha dega
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+
+    sortArray(arr, n - 1);
+}
+int main(int argc, char const *argv[])
+{
+    int arr[5] = {2, 5, 1, 6, 9};
+    sortArray(arr,5);
+    for (int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
