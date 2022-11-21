2a08c913b9ae7fb904a40d3896aa01b700149472
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-21 13:20:17 +0530

    Changes to be committed: 	modified:   Arrays/Binary Seach/eko.cpp

--- Arrays/Binary Seach/eko.cpp
+++ Arrays/Binary Seach/eko.cpp
@@ -69,7 +69,7 @@ int main()
 {
     long long size,height;
     cin >> size>>height;
-    long long arr[size];
+    long long *arr = new long long[size] ;
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
