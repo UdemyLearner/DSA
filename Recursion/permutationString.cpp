3aaf27486087e784b192636d16a84f79db783220
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-22 18:56:12 +0530

    new file:   Recursion/permutationString.cpp 	new file:   Recursion/ratMaze.cpp

---
+++ Recursion/permutationString.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include <vector>
+
+void solve(vector<int> nums, vector<vector<int>> &ans, int index)
+{
+    // base case
+    if (index == nums.size())
+    {
+        ans.push_back(nums);
+        return;
+    }
+    for (int j = index; j < nums.size(); j++)
+    {
+        swap(nums[index], nums[j]);
+        solve(nums, ans, index + 1);
+        // backtrack
+        swap(nums[index], nums[j]);
+    }
+}
+
+vector<vector<int>> permute(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    int index = 0;
+    solve(nums, ans, index);
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1, 2, 3};
+    permute(nums);
+    return 0;
+}
