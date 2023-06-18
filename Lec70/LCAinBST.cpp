f4f53f38bf8b809b0d5576a96e79e6172579110b
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2023-06-18 17:44:46 +0000

    modified:   Lec70/LCAinBST.cpp

--- Lec70/LCAinBST.cpp
+++ Lec70/LCAinBST.cpp
@@ -1,27 +1,37 @@
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //Optimes solution in terms of space
-        if(root==NULL){
-            return NULL;
-        }
-        while(root!=NULL){
-            if(root->val < p->val && root->val < q->val)
-                root = root->right;
-            else if(root->val > p->val && root->val > q->val)
-                root = root->left;
-            else
-                return root;
-        }
-        /*
-        if(root==NULL){
-            return NULL;
-        }
-        if(root->val < p->val && root->val < q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-        if(root->val > p->val && root->val > q->val){
-            return lowestCommonAncestor(root->left,p,q);
-        }
-        return root;   
-        */
-        return root;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    // Optimes solution in terms of space
+    if (root == NULL)
+    {
+        return NULL;
     }
+    while (root != NULL)
+    {
+        if (root->val < p->val && root->val < q->val)
+            root = root->right;
+        else if (root->val > p->val && root->val > q->val)
+            root = root->left;
+        else
+            return root;
+    }
+    /*
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->val < p->val && root->val < q->val){
+        return lowestCommonAncestor(root->right,p,q);
+    }
+    if(root->val > p->val && root->val > q->val){
+        return lowestCommonAncestor(root->left,p,q);
+    }
+    return root;
+    */
+    return root;
+}
