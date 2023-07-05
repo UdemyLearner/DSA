2020f93b3997fe4983bac10029653fd120025a60
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2023-07-05 18:03:48 +0000

    modified:   Lec74 Heap/heap.cpp

--- Lec74 Heap/heap.cpp
+++ Lec74 Heap/heap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 class heap {
 public:
@@ -78,9 +79,9 @@ void heapify(int arr[],int n,int i){
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if(left < n && arr[largest] < arr[left])
+    if(left <= n && arr[largest] < arr[left])
         largest = left;
-    if(right < n && arr[largest] < arr[right])
+    if(right <= n && arr[largest] < arr[right])
         largest = right;
     if(largest != i){
         swap(arr[largest],arr[i]);
@@ -88,7 +89,7 @@ void heapify(int arr[],int n,int i){
     }
 }
 
-void buildMinHeap(vector<int> &arr,int n,int i){
+void buildMinHeap(int arr[],int n,int i){
     int smallest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
@@ -100,6 +101,18 @@ void buildMinHeap(vector<int> &arr,int n,int i){
     if(smallest != i){
         swap(arr[smallest],arr[i]);
         heapify(arr,n,smallest);
+    }
+}
+
+void heapSort(int arr[],int n){
+    int size = n;
+    while(size>1){
+        //step1
+        swap(arr[size],arr[1]);
+        //step2
+        size--;
+        //step3
+        heapify(arr,size,1);
     }
 }
 
@@ -116,7 +129,7 @@ int main(int argc, char const *argv[])
     h.deletefromheap();
     h.print();
 
-    int arr[6] = { -1,51,53,55,52,50};
+    int arr[6] = { -1,54,53,55,52,50};
     int n = 5;
     for(int i = n/2;i>=0;i--){
         heapify(arr,n,i);
@@ -125,5 +138,12 @@ int main(int argc, char const *argv[])
     for(int i = 1;i<=n;i++){
         cout << arr[i] << " ";
     }
+
+    heapSort(arr,n);
+    cout<<"printin array now"<<endl;
+    for(int i = 1;i<=n;i++){
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
