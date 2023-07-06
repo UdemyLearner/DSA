3055207479986dda3f7d5d873935592687f60ba2
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2023-07-06 12:28:26 +0000

    modified:   Lec74 Heap/heap.cpp

--- Lec74 Heap/heap.cpp
+++ Lec74 Heap/heap.cpp
@@ -131,9 +131,29 @@ int main(int argc, char const *argv[])
 
     int arr[6] = { -1,54,53,55,52,50};
     int n = 5;
-    for(int i = n/2;i>=0;i--){
-        heapify(arr,n,i);
-    }
+    // heapify for 1 base indexing
+        for(int i = n/2;i>0;i--){
+            heapify(arr,n,i);
+        }
+        
+    // heapify for 0 base indexing
+    /*    for(int i = (size/2 - 1);i>=0;i--){
+                heapify(ans,size,i);
+            }
+        void heapify(vector<int> &arr,int n,int i){
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+    
+        if(left < n && arr[largest] < arr[left])
+            largest = left;
+        if(right < n && arr[largest] < arr[right])
+            largest = right;
+        if(largest != i){
+            swap(arr[largest],arr[i]);
+            heapify(arr,n,largest);
+        }
+    */
     cout<<"printin array now"<<endl;
     for(int i = 1;i<=n;i++){
         cout << arr[i] << " ";
