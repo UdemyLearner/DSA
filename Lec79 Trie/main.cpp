dfd04ef4b47e4ce0dc424d8ed605f15aeafbacbe
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2023-07-10 07:35:56 +0000

    Lec79 Trie/main.cpp

--- Lec79 Trie/main.cpp
+++ Lec79 Trie/main.cpp
@@ -1,106 +1,113 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class TrieNode{
-public:
-char data;
-TrieNode*children[26];
-bool isTerminal;
-TrieNode(char ch)
+class TrieNode
 {
-    data = ch;
-    for(int i=0; i<26; i++)
+public:
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
+    TrieNode(char ch)
     {
-        children[i] = NULL;
+        data = ch;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isTerminal = false;
     }
-    isTerminal = false;
-}
 };
 
-class Trie {
+class Trie
+{
 public:
-TrieNode*root;  // this trieNode * class will store 3 val a above ;
-Trie() {
-    root = new TrieNode('\n');
-}
-
-void insertWord(TrieNode*root, string word)
-{
-    if(word.length()== 0)
+    TrieNode *root; // this trieNode * class will store 3 val a above ;
+    Trie()
     {
-        root->isTerminal = true;
-        return ;
+        root = new TrieNode('\n');
     }
-    int index = word[0]-'a';
-    TrieNode*child;
-    // case 1 if word is present inside the trie; just shift the child to next 
-    if(root->children[index] != NULL)
-    {
-        child = root->children[index];
-    }
-    // case 2 we have to insert the word inside the trie; and then shift child to next node;
-    else {
-        child = new TrieNode(word[0]);
-        root->children[index] = child;
-    }
-    insertWord(child,word.substr(1)); // recursion ;
-    
-}
 
-void insert(string word) {
-    insertWord(root,word);
+    void insertWord(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return;
         }
-
-// searchWord
-bool searchWord(TrieNode*root,string word)
-{
-    if(word.length()== 0)
-        return root->isTerminal;
-    
-    int index = word[0]-'a';
-    TrieNode*child;
-    // case 1 if perent then continue;
-    if(root->children[index] != NULL)
-    {
-        child = root->children[index];
+        int index = word[0] - 'a';
+        TrieNode *child;
+        // case 1 if word is present inside the trie; just shift the child to next
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        // case 2 we have to insert the word inside the trie; and then shift child to next node;
+        else
+        {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+        insertWord(child, word.substr(1)); // recursion ;
     }
-    else return false;
-    
-    // recursion
-    return searchWord(child,word.substr(1));
-}
 
-bool search(string word) {
-    return searchWord(root,word);
-    
-}
+    void insert(string word)
+    {
+        insertWord(root, word);
+    }
 
-bool code(TrieNode*root,string prefix)
-{
-    if(prefix.length()==0)
+    // searchWord
+    bool searchWord(TrieNode *root, string word)
     {
-        return true;
+        if (word.length() == 0)
+            return root->isTerminal;
+
+        int index = word[0] - 'a';
+        TrieNode *child;
+        // case 1 if present then continue;
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else//absent
+            return false;
+
+        // recursion
+        return searchWord(child, word.substr(1));
     }
-    int index = prefix[0]-'a';
-    TrieNode*child;
-    // case 1 if perent then continue;
-    if(root->children[index] != NULL)
+
+    bool search(string word)
     {
-        child = root->children[index];
+        return searchWord(root, word);
     }
-    else return false;
-    
-    // recursion
-    return code(child,prefix.substr(1));
-}
-bool startsWith(string prefix) {
-    return code(root,prefix);
-}
+
+    bool code(TrieNode *root, string prefix)
+    {
+        if (prefix.length() == 0)
+        {
+            return true;
+        }
+        int index = prefix[0] - 'a';
+        TrieNode *child;
+        // case 1 if present then continue;
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else//absent
+            return false;
+
+        // recursion
+        return code(child, prefix.substr(1));
+    }
+    bool startsWith(string prefix)
+    {
+        return code(root, prefix);
+    }
 };
 
 int main(int argc, char const *argv[])
 {
-    Trie* t;
+    Trie *t;
     t->insert("apple");
     return 0;
 }
