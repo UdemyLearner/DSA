aa92463a1c0b0d4b3079b3150fdabfbfa86291bf
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2023-07-16 08:03:10 +0000

    Lec85 Graphs/main.cpp

--- Lec85 Graphs/main.cpp
+++ Lec85 Graphs/main.cpp
@@ -1,8 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
+template <typename T>
+class graph{
+    public:
+        unordered_map<T,list<T>> adj;
+
+    void addEdge(T u,T v,bool direction){
+        //direction = 0 -> undirected
+
+        adj[u].push_back(v);
+        if(direction==0)
+            adj[v].push_back(u);
+
+    }
+
+    void print(){
+        for(auto i:adj){
+            cout<<i.first<<" -> ";
+            for(auto j:i.second){
+                cout<<j<<" , ";
+            }
+            cout<<endl;
+        }
+    }
+
+};
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    int n;
+    cout<<"no of nodes"<<endl;
+    cin>>n;
+
+    int m;
+    cout<<"no edges"<<endl;
+    cin>>m;
+
+    graph<int> g;
+
+    for (size_t i = 0; i < m; i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    
+    g.print();
+
     return 0;
 }
